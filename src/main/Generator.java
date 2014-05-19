90192dffa9290f636b654032ba405666f5786dd1
Falros <falrosspam@gmail.com>
2014-05-19 13:01:03 +0200

    Modified code to allow for generation of multiple pw in a range of sizes.

--- src/main/Generator.java
+++ src/main/Generator.java
@@ -13,25 +13,75 @@ public class Generator {
     }
 
     public String createPW(int length) { // Suppose length 8-24
-        String out = "";
-        if (special) {
-            // Format : rrll-rrll-rrll......
-            // TODO : normalize length
+        if (!special) {
+            return createPWnoSpecial(length);
         } else {
-            // Format : lllrrrrllrrrlll.....
-            int handAlternator = 1;
-            while (length > 0) {
-                String tempAlph = alphabet[handAlternator++ % 2];
-                int sequenceLength = length <= 4 ? length : rand.nextInt(3) + 2; // 2
-                                                                                 // -
-                                                                                 // 4
-                for (int i = 0; i < sequenceLength; i++) {
-                    out += tempAlph.charAt(rand.nextInt(tempAlph.length()));
-                }
-                length -= sequenceLength;
+            String out = "";
+            switch (length) {
+            case 9:
+                // llll-rrrr
+                out += fillSequence(4, alphabet[0]);
+                out += "-";
+                out += fillSequence(4, alphabet[1]);
+                return out;
+            case 13:
+                // rrrlll-lllrrr
+                out += fillSequence(3, alphabet[1]);
+                out += fillSequence(3, alphabet[0]);
+                out += "-";
+                out += fillSequence(3, alphabet[0]);
+                out += fillSequence(3, alphabet[1]);
+                return out;
+            case 14:
+                // llll-rrrr-llll
+                out += fillSequence(4, alphabet[0]);
+                out += "-";
+                out += fillSequence(4, alphabet[1]);
+                out += "-";
+                out += fillSequence(4, alphabet[0]);
+                return out;
+            case 15:
+                // llll-rrrlll-rrrr
+                out += fillSequence(4, alphabet[0]);
+                return out;
+            case 18:
+                // rrrlll-rrrr-lllrrr
+                return out;
+            case 19:
+                // llll-rrrr-llll-rrrr
+                return out;
+            case 20:
+                // rrrlll-rrrlll-rrrlll
+                return out;
+            case 24:
+                // llll-rrrr-llll-rrrr-llll
+                return out;
+            default:
+                return createPWnoSpecial(length);
+
             }
         }
+    }
+
+    public String createPWnoSpecial(int length) {
+        String out = "";
+        // Format : lllrrrrllrrrlll.....
+        int handAlternator = 1;
+        while (length > 0) {
+            String tempAlph = alphabet[handAlternator++ % 2];
+            int sequenceLength = length <= 4 ? length : rand.nextInt(3) + 2;
+            out += fillSequence(sequenceLength, tempAlph);
+            length -= sequenceLength;
+        }
         return out;
+    }
+
+    public String fillSequence(int size, String a) {
+        String r = "";
+        for (int i = 0; i < size; i++) {
+            r += a.charAt(rand.nextInt(a.length()));
+        }
+        return r;
     }
 
 }
