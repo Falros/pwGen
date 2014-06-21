2e0d42ccc3907fee47b5221080d0a53a7a7215a8
Falros <falrosspam@gmail.com>
2014-06-21 22:05:22 +0200

    Update of CLI text, added card option.

--- src/main/Gui.java
+++ src/main/Gui.java
@@ -4,7 +4,7 @@ public class Gui {
     public static void main(String[] args) {
         if (args.length == 0) {
             Generator gen = new Generator(false);
-            System.out.println("Passwords with no special characters :\n");
+            System.out.println("\n Passwords with no special characters :\n");
             for (int i = 8; i <= 24; i++) {
                 String s = gen.createPW(i);
                 if (s != null) {
@@ -25,16 +25,25 @@ public class Gui {
                     System.out.println(s.length() + "  |  " + s);
                 }
             }
+            System.out.println("\n");
         } else {
             if (args[0].equals("nofuss")) {
                 Generator gen = new Generator(true);
+                System.out.println("\n");
                 System.out.println(gen.createPW(19));
                 gen.setSpecial(false);
                 System.out.println(gen.createPW(12));
+                System.out.println("\n");
+            } else if (args[0].equals("card")) {
+                Generator gen = new Generator(true);
+                System.out.println("\n");
+                for (int i = 0; i < 5; i++) {
+                    System.out.println(gen.createPW(19));
+                }
+                System.out.println("\n");
             } else {
-                Generator gen = new Generator(Boolean.parseBoolean(args[0]));
-                gen.setSpecific(true);
-                System.out.println(gen.createPW(Integer.parseInt(args[1])));
+                System.out
+                        .println("Illegal arguments. Valid arguments are nofuss and card.");
             }
         }
     }
